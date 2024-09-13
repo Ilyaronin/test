a20b0262e4006670c642f21dab86a087def9b3a0
Ilyaronin <novozhilov2007@internet.ru>
2024-09-13 14:58:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
-
+import os
+import random
+import requests
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -24,6 +27,15 @@ async def add(ctx, left: int, right: int):
     await ctx.send(left + right)
 
 @bot.command()
+async def mem(ctx):
+    images = os.listdir('images')    
+    img_name = random.choice(images)
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+ 
+    await ctx.send(file=picture)  
+
+@bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
@@ -34,6 +46,29 @@ async def repeat(ctx, times: int, content='repeating...'):
     for i in range(times):
         await ctx.send(content)
 
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
 
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
 
 bot.run("token")
